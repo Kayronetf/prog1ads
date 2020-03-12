9fa91248d7e5c637172a4b22679598e9e9eae8fc
kayronetf <kayronefilgueiras@ads.fiponline.edu.br>
2020-03-12 16:16:14 -0300

    questao 10

---
+++ questao 10.py
@@ -0,0 +1,4 @@
+salario = float(input("qual é o salario do funcionario: R$"))
+aumento = float(input("digite o valor do aumento: "))
+novo = salario + (salario * aumento)
+print("o novo salario do funcionario é: {}".format(novo))
