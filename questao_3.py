853797a9591a009587d5603e3471a0e64c7d4c55
Kayronetf <61851823+Kayronetf@users.noreply.github.com>
2020-03-12 13:02:28 -0300

    Delete questao_3.py

--- questao_3.py
+++
@@ -1,6 +0,0 @@
-inicial = int(input("digite o km inicial do seu percurso: "))
-final = int(input("digite o km final do seu percurso: "))
-total_km = final - inicial
-km_por_l = float(input("digite quantos km por litro o seu carro faz: "))
-media = total_km / km_por_l
-print("a media que seu carro fez no percurso foi: {:.2f}".format(media))
