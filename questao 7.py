6e3aadd27a216d7a2c355144466fd36fca4c1e30
kayronetf <kayronefilgueiras@ads.fiponline.edu.br>
2020-03-12 16:10:50 -0300

    questao 7

---
+++ questao 7.py
@@ -0,0 +1,18 @@
+largura1 = float(input("digite a largura do primeiro comodo: "))
+comprimento1 = float(input("digite o comprimento do primeiro comodo: "))
+area1 = largura1 * comprimento1
+print("a area 1 é igual a: {}".format(area1))
+largura2 = float(input("digite a largura do segundo comodo: "))
+comprimento2 = float(input("digite o comprimento do segundo comodo: "))
+area2 = largura2 * comprimento2
+print("a area 2 é igual a: {}".format(area2))
+largura3 = float(input("digite a largura do terceiro comodo: "))
+comprimento3 = float(input("digite o comprimento do terceiro comodo: "))
+area3 = largura3 * comprimento3
+print("a area 3 é igual a: {}".format(area3))
+largura4 = float(input("digite a largura do quarto comodo: "))
+comprimento4 = float(input("digite a largura do quarto comodo: "))
+area4 = largura4 * comprimento4
+print("a area 4 é igual a: {}".format(area4))
+area_total = area1 + area2 + area3 + area4
+print("a area total da casa é igual a: {}".format(area_total))
