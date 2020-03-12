ad361b27612d65ffb4d656b631053a4c365c8d78
kayronetf <kayronefilgueiras@ads.fiponline.edu.br>
2020-03-12 13:57:56 -0300

    questao 3

---
+++ questao 3.py
@@ -0,0 +1,4 @@
+litros= float(input("digite quantos litros de gasolina contém no seu carro: "))
+distancia_final = float(input("digite qual a distancia final do seu percuso: "))
+distancia_inicial = float(input("digite o km do do seu ponto de partida: "))
+consumo = (distancia_final - distancia_inicial) / litros
